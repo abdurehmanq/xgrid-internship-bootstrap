b2979eaa6fcde1069f638b8702c055a8a0fcbbe0
Abdur Rehman <abdur.rehman@xgrid.co>
2026-03-03 16:25:50 +0500

    feat: add week 2 terraform project and resolve health script conflict

---
+++ xgrid-internship-terraform/main.tf
@@ -0,0 +1,46 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "xgrid-intern-sg"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "xgrid-week2-server"
+    Role = "SRE-Intern"
+  }
+}
