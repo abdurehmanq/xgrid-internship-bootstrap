cf676ad385c4091781fbca3391cf5235e7d6abb1
abdurehmanq <abdur.rehman@xgrid.co>
2026-03-26 21:01:57 +0500

    Update outputs.tf

--- week4/3tier-app/modules/database/outputs.tf
+++ week4/3tier-app/modules/database/outputs.tf
@@ -7,3 +7,9 @@ output "database_name" {
   description = "The name of the initial database"
   value       = aws_db_instance.postgres_db.db_name
 }
+
+# This grabs the randomly generated secret ARN directly from RDS
+output "db_secret_arn" {
+  description = "The ARN of the RDS-managed Secrets Manager secret"
+  value       = aws_db_instance.postgres_db.master_user_secret[0].secret_arn
+}
