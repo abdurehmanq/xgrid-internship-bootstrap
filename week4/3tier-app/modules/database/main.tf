cae0ea641712491d935195fb5dca7a938e866824
abdurehmanq <abdur.rehman@xgrid.co>
2026-03-26 21:01:13 +0500

    Update main.tf

--- week4/3tier-app/modules/database/main.tf
+++ week4/3tier-app/modules/database/main.tf
@@ -8,19 +8,16 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 
 # 2. Standard Free-Tier Eligible PostgreSQL Instance
 resource "aws_db_instance" "postgres_db" {
-  identifier             = "${var.vpc_name}-database"
-  engine                 = "postgres"
+  identifier                  = "${var.vpc_name}-database"
+  engine                      = "postgres"
+  instance_class              = "db.t3.micro" 
+  allocated_storage           = 20            
+  db_name                     = var.db_name
+  username                    = var.db_master_username
+  manage_master_user_password = true      
   
-  # Let the sandbox pick its own default supported Postgres version!
-  
-  instance_class         = "db.t3.micro" 
-  allocated_storage      = 20            
-  db_name                = var.db_name
-  username               = var.db_master_username
-  manage_master_user_password = true     
-  
-  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
-  vpc_security_group_ids = [var.database_sg_id]
+  db_subnet_group_name        = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids      = [var.database_sg_id]
   
   iam_database_authentication_enabled = true
   skip_final_snapshot                 = true
